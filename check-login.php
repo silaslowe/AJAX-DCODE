53deedb4a3f2dd297814584acb47a99712cfac63
Silas Lowe <60553895+silaslowe@users.noreply.github.com>
2020-07-11 15:00:06 -0500

    Add files via upload

--- check-login.php
+++ check-login.php
@@ -1,35 +1,36 @@
 <?php
-$username = isset($_POST['username']) ? $_POST['username'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
 
-$ok = true;
-$messages = array();
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $password = isset($_POST['password']) ? $_POST['password'] : '';
 
-if(!isset($username) || empty($username)) {
-    $ok = false;
-    $messages[] = 'Username cannot be empty';
-}
+    $ok = true;
+    $messages = array();
 
-if(!isset($password) || empty($password)) {
-    $ok = false;
-    $messages[] = 'Username cannot be empty';
-}
+    if ( !isset($username) || empty($username) ) {
+        $ok = false;
+        $messages[] = 'Username cannot be empty!';
+    }
 
-if($ok) {
-    if($username === 'dcode' && $password === 'dcode') {
-       $ok = true; 
-       $messages[] = 'Successful login'; 
-       } else {
-           $ok = false;
-           $messages[] = "Incorrect username/passwprd combination!";
-       }
-}
+    if ( !isset($password) || empty($password) ) {
+        $ok = false;
+        $messages[] = 'Password cannot be empty!';
+    }
 
-echo json_encode(
-    array(
-        'ok' => $ok,
-        'messages' => $messages
-    )
+    if ($ok) {
+        if ($username === 'dcode' && $password === 'dcode') {
+            $ok = true;
+            $messages[] = 'Successful login!';
+        } else {
+            $ok = false;
+            $messages[] = 'Incorrect username/password combination!';
+        }
+    }
+
+    echo json_encode(
+        array(
+            'ok' => $ok,
+            'messages' => $messages
+        )
     );
 
-    ?>
+?>
